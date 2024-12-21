86f967a8ad53201670ff70cbff83e11e8eab2656
Anshu Sinha <anshusinha872@gmail.com>
2024-12-21 18:07:36 +0530

    Refactor GroupScreen to display groups instead of transactions; implement FlatList for group categories and improve layout

--- SplitKaroApp/src/screens/GroupScreen.js
+++ SplitKaroApp/src/screens/GroupScreen.js
@@ -2,7 +2,6 @@ import React from "react";
 import {
   FlatList,
   Image,
-  ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -11,7 +10,7 @@ import {
 import { Ionicons } from "@expo/vector-icons";
 
 const GroupScreen = () => {
-  const transactions = [
+  const groups = [
     {
       id: "1",
       category: "Group-1",
@@ -44,99 +43,80 @@ const GroupScreen = () => {
       date: "20th Jan",
       icon: require("../../assets/icons/shopping-bag.png"),
     },
+    {
+      id: "5",
+      category: "Group-5",
+      description: "Buy some grocery",
+      amount: "₹500",
+      date: "20th Jan",
+      icon: require("../../assets/icons/shopping-bag.png"),
+    },
   ];
+
   const renderItem = ({ item }) => (
-    <View>
-      <View style={styles.transaction}>
-        <View style={styles.transactionIconContainer}>
-          <Image style={styles.transactionIcon} source={item.icon} />
+    <View style={styles.group}>
+      <View style={{width: "100%", flexDirection: "row"}}>
+        <View style={styles.groupIconContainer}>
+          <Image style={styles.groupIcon} source={item.icon} />
         </View>
-        <View style={styles.transactionDetails}>
-          <View style={styles.transactionCategoryDetails}>
-            <Text style={styles.transactionCategory}>{item.category}</Text>
+        <View style={styles.groupDetails}>
+          <View style={styles.groupCategoryDetails}>
+            <Text style={styles.groupCategory}>{item.category}</Text>
           </View>
-          <View style={{ alignItems: "flex-end" }}>
-            <Text style={styles.transactionDate}>{item.date}</Text>
-            <Text style={styles.transactionAmount}>{item.amount}</Text>
+          <View style={styles.groupDateContainer}>
+            <Text style={styles.groupDate}>{item.date}</Text>
+            <Text style={styles.groupAmount}>{item.amount}</Text>
           </View>
         </View>
       </View>
-      <View style={{ marginLeft: 50 }}>
-        <Text style={styles.transactionDescription}>{item.description}</Text>
-        <Text style={styles.transactionDescription}>{item.description}</Text>
-        <Text style={styles.transactionDescription}>{item.description}</Text>
+      <View>
+        <View style={styles.groupDescriptionContainer}>
+          <Text style={styles.groupDescription}>{item.description}</Text>
+          <Text style={styles.groupDescription}>{item.description}</Text>
+          <Text style={styles.groupDescription}>{item.description}</Text>
+        </View>
       </View>
     </View>
   );
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerText}>Groups</Text>
-        <TouchableOpacity style={styles.btn}>
-          <Text style={styles.btnText}>Create a Group</Text>
+        <TouchableOpacity style={styles.createGroupBtn}>
+          <Text style={styles.createGroupText}>Create a Group</Text>
         </TouchableOpacity>
       </View>
-      <ScrollView
-        horizontal={true}
+
+      <FlatList
+        horizontal
+        data={["All", "Home", "Trip", "Personal", "Others"]}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.grpCategorybtn}>
+            <Text style={styles.grpCategorybtnText}>{item}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        style={styles.grpCategoryListContainer}
         showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.grpCategoryListContainer}
-      >
-        <TouchableOpacity style={styles.grpCategorybtn}>
-          <Text style={styles.grpCategorybtnText}>All</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.grpCategorybtn}>
-          <Text style={styles.grpCategorybtnText}>Home</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.grpCategorybtn}>
-          <Text style={styles.grpCategorybtnText}>Trip</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.grpCategorybtn}>
-          <Text style={styles.grpCategorybtnText}>Personal</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.grpCategorybtn}>
-          <Text style={styles.grpCategorybtnText}>Personal</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.grpCategorybtn}>
-          <Text style={styles.grpCategorybtnText}>Personal</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.grpCategorybtn}>
-          <Text style={styles.grpCategorybtnText}>Personal</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[styles.grpCategorybtn, styles.grpCategorybtnActive]}
-        >
-          <Text
-            style={[styles.grpCategorybtnText, styles.grpCategorybtnTextActive]}
-          >
-            Others
-          </Text>
-        </TouchableOpacity>
-      </ScrollView>
+      />
 
       <View style={styles.subHeader}>
-        <Text>
-          Overall,you are owned{"  "}
-          <Text style={{ fontWeight: "bold" }}>₹500</Text>
+        <Text style={styles.subHeaderText}>
+          Overall, you are owned <Text style={styles.boldText}>₹500</Text>
         </Text>
         <TouchableOpacity style={styles.btn}>
           <Ionicons name="options-outline" size={24} color="white" />
         </TouchableOpacity>
       </View>
-      <ScrollView>
 
-      <View style={
-        {height: 500}
-      }>
-        <FlatList
-          data={transactions}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-          contentContainerStyle={styles.transactionListContainer}
-          alwaysBounceVertical={true}
-          showsVerticalScrollIndicator={false}
-        />
-      </View>
-      </ScrollView>
+      <FlatList
+        data={groups}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.groupListContainer}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
@@ -144,104 +124,118 @@ const GroupScreen = () => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
-    padding: 10,
+    padding: 16,
   },
   header: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    width: "100%",
   },
   headerText: {
     fontSize: 24,
     fontWeight: "bold",
+    color: "#333",
   },
-  btn: {
-    borderRadius: 5,
+  createGroupBtn: {
     backgroundColor: "#6C63FF",
-    paddingVertical: 5,
-    paddingHorizontal: 10,
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 5,
   },
-  btnText: {
+  createGroupText: {
     color: "#fff",
+    fontSize: 14,
   },
   grpCategoryListContainer: {
     flexDirection: "row",
-    paddingVertical: 10,
-    paddingHorizontal: 5,
-    height: 40,
+    marginTop: 16,
+    height: 50,
   },
   grpCategorybtn: {
-    borderWidth: 2,
-    borderColor: "#6C63FF",
-    paddingVertical: 3,
-    paddingHorizontal: 10,
-    borderRadius: 5,
-    marginRight: 5,
-    height: 30,
+    backgroundColor: "#F5F5F5",
+    paddingHorizontal:10,
+    paddingVertical: 6,
+    textAlign: "center",
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: 12,
+    borderRadius: 20,
   },
   grpCategorybtnText: {
     color: "#6C63FF",
-    fontSize: 12,
-  },
-  grpCategorybtnActive: {
-    backgroundColor: "#6C63FF",
-  },
-  grpCategorybtnTextActive: {
-    color: "#fff",
   },
   subHeader: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    width: "100%",
-    marginTop: 10,
+    marginTop: 16,
   },
-  transactionListContainer: {
-    marginTop: 10,
-    paddingBottom: 150,
+  subHeaderText: {
+    fontSize: 12,
+    color: "#333",
   },
-  transaction: {
-    flexDirection: "row",
-    alignItems: "center",
-    borderRadius: 10,
-    marginTop: 10,
+  boldText: {
+    fontWeight: "bold",
+    color: "#FD5662",
+  },
+  btn: {
+    backgroundColor: "#6C63FF",
+    padding: 8,
+    borderRadius: 50,
+  },
+  groupListContainer: {
+    paddingTop: 16,
+    paddingBottom: 200,
+  },
+  group: {
+    flexDirection: "column",
     backgroundColor: "#FFF",
-  },
-  transactionIconContainer: {
-    backgroundColor: "#FCEED4",
+    borderRadius: 10,
+    marginBottom: 16,
     padding: 5,
+  },
+  groupIconContainer: {
+    backgroundColor: "#FCEED4",
+    padding: 8,
     borderRadius: 10,
   },
-  transactionIcon: {
+  groupIcon: {
     width: 30,
     height: 30,
   },
-  transactionDetails: {
+  groupDetails: {
     flexDirection: "row",
     justifyContent: "space-between",
+    marginLeft: 12,
     flex: 1,
-    marginLeft: 10,
   },
-  transactionCategoryDetails: {
-    flex: 1,
+  groupCategoryDetails: {
     flexDirection: "row",
     alignItems: "center",
   },
-  transactionCategory: {
+  groupCategory: {
     fontSize: 16,
+    fontWeight: "bold",
+    color: "#333",
   },
-  transactionDescription: {
+  groupDateContainer: {
+    alignItems: "flex-end",
+  },
+  groupDate: {
+    fontSize: 12,
     color: "#A1A1A1",
   },
-  transactionAmount: {
+  groupAmount: {
     fontSize: 16,
     fontWeight: "bold",
     color: "#FD5662",
   },
-  transactionDate: {
+  groupDescriptionContainer: {
+    marginLeft: 50,
+  },
+  groupDescription: {
+    fontSize: 14,
     color: "#A1A1A1",
-    fontSize: 12,
   },
 });
 
